2c86a0c0cd3717af1bdb600d0460b88968eb2aa8
unknown <pamela.lotriet@gmail.com>
2022-09-24 10:53:33 +0200

    Wakeup controller added

---
+++ portfolio_back/portfolio_back/Controllers/WakeUpController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace portfolio_back.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class WakeUpController: ControllerBase
+    {
+
+        [HttpGet]
+        [Route("/wakeUp")]
+        public string wakeup()
+        {
+            return "Service has been woken up";
+        }
+    }
+}
